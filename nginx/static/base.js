151100316fa9446d78f69299ce98be762fdcc2e1
retrontology <retrontology@gmail.com>
2025-07-11 12:01:15 -0500

    partially working updates

--- nginx/static/base.js
+++ nginx/static/base.js
@@ -1,3 +1,6 @@
+var frames = []
+const img_prefix = '/media/'
+
 function slugify(value, allowUnicode = false) {
 
     value = String(value);
@@ -29,8 +32,28 @@ async function getRegions() {
 
 }
 
-function changeRegion() {
+async function getRegionFrames(region) {
+    const url = '/region/' + region;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
+async function changeRegion() {
+    var region = document.getElementById('region-select').value;
+    frames = await getRegionFrames(region);
+
+    var slider = document.getElementById('date-slider');
+    slider.min = 0;
+    slider.max = frames.length - 1;
+    slider.value = 0;
 }
 
 async function populateRegions() {
@@ -43,26 +66,41 @@ async function populateRegions() {
         option.text = regions[i];
         select.appendChild(option);
     }
-    //changeRegion();
+    await changeRegion();
+    changeDate();
 }
 
 function changeDate() {
+    var slider = document.getElementById('date-slider');
+    var frame = frames[slider.value];
 
+    document.getElementById('date').innerHTML = frame.date;
+    document.getElementById('title').innerHTML = frame.title;
+    document.getElementById('description').innerHTML = frame.description;
+    document.getElementById('map').src = img_prefix + frame.path;
 }
 
 function prevDate() {
-    
+    var slider = document.getElementById('date-slider');
+    if (parseInt(slider.value) > parseInt(slider.min)) {
+        slider.value--;
+        changeDate();
+    }
 }
 
 function nextDate() {
-    
+    var slider = document.getElementById('date-slider');
+    if (parseInt(slider.value) <= parseInt(slider.max)) {
+        slider.value++;
+        changeDate();
+    }
 }
 
 document.onkeyup = function(e) {
-    if (e.key == 37){
+    if (e.key == "ArrowLeft"){
         prevDate();
     }
-    else if (e.key == 39){
+    else if (e.key == "ArrowRight"){
         nextDate();
     }
 };
