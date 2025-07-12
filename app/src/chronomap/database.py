1d9b08bcf6977d1e94ba44417448117f94aa2768
retrontology <retrontology@gmail.com>
2025-07-12 10:58:40 -0500

    fix db connection

--- app/src/chronomap/database.py
+++ app/src/chronomap/database.py
@@ -5,25 +5,14 @@ from .frame import Frame
 
 FRAME_TABLE = "atlas_frame"
 
-print(
-   f"""
-   connection = mysql.connector.connect(
-      host={config.MYSQL_HOST},
-      port={config.MYSQL_PORT},
-      user={config.MYSQL_USER},
-      password={config.MYSQL_PASS},
-      database={config.MYSQL_DB},
+def get_db() -> mysql.connector.connection.MySQLConnection:
+   return mysql.connector.connect(
+      host=config.MYSQL_HOST,
+      port=config.MYSQL_PORT,
+      user=config.MYSQL_USER,
+      password=config.MYSQL_PASS,
+      database=config.MYSQL_DB,
    )
-   """
-)
-
-connection = mysql.connector.connect(
-    host=config.MYSQL_HOST,
-    port=config.MYSQL_PORT,
-    user=config.MYSQL_USER,
-    password=config.MYSQL_PASS,
-    database=config.MYSQL_DB,
-)
 
 
 def get_regions() -> list[str]:
@@ -33,12 +22,14 @@ def get_regions() -> list[str]:
     Return:
        list[str]: A list of unique regions.
     """
+    connection = get_db()
     cursor = connection.cursor()
     query = f"SELECT UNIQUE(region) FROM {FRAME_TABLE};"
     cursor.execute(query)
     result = cursor.fetchall()
     regions = [row[0] for row in result]
     cursor.close()
+    connection.close()
     return regions
 
 
@@ -51,10 +42,12 @@ def get_region_frames(region) -> list[Frame]:
     Return:
        list[Frame]: A list of Frame objects.
     """
+    connection = get_db()
     cursor = connection.cursor()
     query = f"SELECT * FROM {FRAME_TABLE} WHERE region = %s ORDER BY date ASC;"
     cursor.execute(query, (region,))
     result = cursor.fetchall()
     frames = [Frame(*row) for row in result]
     cursor.close()
+    connection.close()
     return frames
